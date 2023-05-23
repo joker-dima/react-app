113a4b841ce45f7336b8c87b5e46740adf64eb1d
Dmitriy Malyshev <joker-dima@yandex.ru>
2023-05-23 20:45:28 +0300

    Чат верстаем

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,12 +1,24 @@
 import React from "react"
 import style from './Chat.module.css'
+import { NavLink } from "react-router-dom"
 
 
 let Chat = () => {
 
     return (
-        <div>
-            CHAT
+
+        <div className={style.chat_wrapper}> 
+            <div className={style.dialogs}>
+                <NavLink className={navData => navData.isActive ? style.active : style.dialog} >Франко Коломбо</NavLink>
+                <NavLink  className={navData => navData.isActive ? style.active : style.dialog} >Сильвестер Сталоне</NavLink>
+                <NavLink  className={navData => navData.isActive ? style.active : style.dialog} >Саша Курицын</NavLink>
+                <NavLink className={navData => navData.isActive ? style.active : style.dialog} >Дом.работница</NavLink>
+            </div>
+<div className={style.messages}>
+<div className={style.message}>Хей Бро, как там?</div>
+<div className={style.message}>Снимем еще одних неудержимых?</div>
+<div className={style.message}>Уот так вот, Сашок!</div>
+</div>
         </div>
     )
 }
